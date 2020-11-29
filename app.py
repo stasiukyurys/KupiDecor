a58c545db04dac230766ffb0e9c67785f04d768f
stasiukyurys@gmail.com <stasiukyurys@gmail.com>
2020-11-29 20:40:58 +0300

    Начало разработки!

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello tem'
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
